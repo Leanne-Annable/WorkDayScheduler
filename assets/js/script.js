ba5e48cddd583a97872f3a55d135006cb9faf78c
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-01-20 15:40:35 +0000

    added a way to pull from local storage for each time slot

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,4 +28,15 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     });
 
+    // pull any previously saved data from the local storage.
+    // need to reference each hour individually
+    $("#time9 .description").val(localStorage.getItem("time9"));
+    $("#time10 .description").val(localStorage.getItem("time10"));
+    $("#time11 .description").val(localStorage.getItem("time11"));
+    $("#time12 .description").val(localStorage.getItem("time12"));
+    $("#time13 .description").val(localStorage.getItem("time13"));
+    $("#time14 .description").val(localStorage.getItem("time14"));
+    $("#time15 .description").val(localStorage.getItem("time15"));
+    $("#time16 .description").val(localStorage.getItem("time16"));
+    $("#time17 .description").val(localStorage.getItem("time17"));
 })
